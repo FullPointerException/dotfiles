802c7482bce83ff74e638d9c0dfeed8dea771f66
Full Pointer Exception <fullpointerxception@gmail.com>
2024-06-27 10:07:41 -0500

    Added bin folder

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
+echo "Setting up $HOME/.local/bin"
+mkdir -p $HOME/.local/bin
+cp ./bin/* $HOME/.local/bin
+
+echo "Setting up starship"
 mkdir -p $HOME/.config/starship
 cp ./starship/starship/starship.toml $HOME/.config/starship/starship.toml
+
