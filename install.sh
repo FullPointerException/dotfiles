a965d466ad221da84a1a71b7dd87126698b8b0b0
Full Pointer Exception <fullpointerxception@gmail.com>
2024-06-27 09:38:33 -0500

    Basic scripts, starship config

---
+++ install.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+mkdir -p $HOME/.config/starship
+cp ./starship/starship/starship.toml $HOME/.config/starship/starship.toml
---
+++ sync.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+cp $HOME/.config/starship/starship.toml ./starship/starship.toml
