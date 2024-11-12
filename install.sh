cef7e9595c9c218d549bbbdfa9ee2db45a7d396f
Full Pointer Exception <fullpointerxception@gmail.com>
2024-11-12 13:33:20 -0600

    Made shellcheck happy

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 echo "Setting up $HOME/.local/bin"
-mkdir -p $HOME/.local/bin
-cp ./bin/* $HOME/.local/bin
+mkdir -p "$HOME/.local/bin"
+cp ./bin/* "$HOME/.local/bin"
 
 echo "Setting up starship"
-mkdir -p $HOME/.config/starship
-cp ./starship/starship.toml $HOME/.config/starship/starship.toml
+mkdir -p "$HOME/.config/starship"
+cp ./starship/starship.toml "$HOME/.config/starship/starship.toml"
 
--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 echo "Syncing changes from $HOME/.local/bin"
-cp $HOME/.local/bin/jj-git-config-local-repo ./bin/jj-git-config-local-repo
-cp $HOME/.local/bin/jj-git-delete-tag ./bin/jj-git-delete-tag
+cp "$HOME/.local/bin/jj-git-config-local-repo" ./bin/jj-git-config-local-repo
+cp "$HOME/.local/bin/jj-git-delete-tag" ./bin/jj-git-delete-tag
 
 echo "Syncing starship changes"
-cp $HOME/.config/starship/starship.toml ./starship/starship.toml
+cp "$HOME/.config/starship/starship.toml" ./starship/starship.toml
